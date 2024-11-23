e00d231dde5bb4425d318ab99e3ec4207430e190
Byter <vitazagrebelnyh@gmail.com>
2024-11-23 16:44:11 +1000

    adding fields to user table

--- user/models.py
+++ user/models.py
@@ -2,4 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    money = models.IntegerField(default=0)
+    maximumMoney = models.IntegerField(default=0)
+    heroStrength = models.IntegerField(default=1)
+    heroSpeed = models.IntegerField(default=1)
+    heroDurability = models.IntegerField(default=1)
