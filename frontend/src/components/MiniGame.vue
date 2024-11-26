a67ca6241afdb046585590ab7075c43fe8ea31f1
ermolaevaalenaandreevna <alyonaerm13@gmail.com>
2024-11-26 17:58:46 +1000

    minigame 2

--- frontend/src/components/MiniGame.vue
+++ frontend/src/components/MiniGame.vue
@@ -3,21 +3,63 @@
 export default {
   data() {
     return {
-      gameStarted: false,
+      /*gameStarted: false,
       gameCountdownVisible: false,
       countdown: 3,
       timeLeft: 30,
       clickCount: 0,
       gameEnded: false,
-      resultMessage: ''
+      resultMessage: ''*/
+      words: ["апельсин", "банан", "киви", "груша", "яблоко"],
+      currentWordIndex: 0,
+      currentWord: "",
+      userInput: "",
+      correctCount: 0,
+      timeRemaining: 10,
+      gameOver: false,
+      timer: null,
+      countdown: 3,
+      gameCountdownVisible: false,
     }
     },
     methods: {
+        // startGame() {
+        //     this.gameStarted = false;
+        //     this.gameEnded = false;
+        //     this.clickCount = 0;
+        //     this.timeLeft = 30;
+        //     this.countdown = 3;
+        //     this.gameCountdownVisible = true;
+        //
+        //     const countdownInterval = setInterval(() => {
+        //         if (this.countdown > 0) {
+        //             this.countdown--;
+        //         } else {
+        //             clearInterval(countdownInterval);
+        //             this.gameCountdownVisible = false;
+        //             this.startTimer();
+        //         }
+        //     }, 1000);
+        // },
+        // startTimer() {
+        //     this.gameStarted = true;
+        //     const timerInterval = setInterval(() => {
+        //         if (this.timeLeft > 0) {
+        //             this.timeLeft--;
+        //         } else {
+        //             clearInterval(timerInterval);
+        //             this.endGame();
+        //         }
+        //     }, 1000);
+        // },
         startGame() {
-            this.gameStarted = false;
-            this.gameEnded = false;
-            this.clickCount = 0;
-            this.timeLeft = 30;
+            this.gameOver = false;
+            this.correctCount = 0;
+            this.currentWordIndex = 0;
+            this.timeRemaining = 10;
+            this.userInput = "";
+            this.nextWord();
+            this.startTimer();
             this.countdown = 3;
             this.gameCountdownVisible = true;
 
@@ -30,34 +72,53 @@ export default {
                     this.startTimer();
                 }
             }, 1000);
-        },
+          },
         startTimer() {
-            this.gameStarted = true;
-            const timerInterval = setInterval(() => {
-                if (this.timeLeft > 0) {
-                    this.timeLeft--;
-                } else {
-                    clearInterval(timerInterval);
-                    this.endGame();
-                }
-            }, 1000);
-        },
-        registerClick() {
-            this.clickCount++;
-        },
-        endGame() {
-            this.gameStarted = false;
-            this.gameEnded = true;
-            let result = false;
-            if (this.clickCount > 100) {
-              result = true;
-              this.resultMessage = "Поздравляем! Вы набрали " + this.clickCount + " кликов!";
-            } else {
-              result = false;
-              this.resultMessage = "Вот и все! Вы набрали только " + this.clickCount + " кликов.";
+          this.timer = setInterval(() => {
+            this.timeRemaining--;
+            if (this.timeRemaining <= 0) {
+              clearInterval(this.timer);
+              this.currentWordIndex++; // Переходим к следующему слову
+              this.nextWord(); // Загружаем следующее слово
             }
-            this.$emit('game-finished', result);
+          }, 1000);
         },
+        nextWord() {
+          if (this.currentWordIndex < this.words.length) {
+            this.currentWord = this.words[this.currentWordIndex];
+            this.userInput = "";
+            this.timeRemaining = 10; // Сбрасываем время
+          } else {
+            this.gameOver = true; // Игра окончена
+            clearInterval(this.timer); // Остановить таймер
+          }
+        },
+        checkWord() {
+          if (this.userInput === this.currentWord) {
+            this.correctCount++;
+          } else {
+            this.incorrectCount++;
+          }
+          this.currentWordIndex++; // Переходим к следующему слову
+          this.nextWord(); // Загружаем следующее слово
+        },
+
+        // registerClick() {
+        //     this.clickCount++;
+        // },
+        // endGame() {
+        //     this.gameStarted = false;
+        //     this.gameEnded = true;
+        //     let result = false;
+        //     if (this.clickCount > 100) {
+        //       result = true;
+        //       this.resultMessage = "Поздравляем! Вы набрали " + this.clickCount + " кликов!";
+        //     } else {
+        //       result = false;
+        //       this.resultMessage = "Вот и все! Вы набрали только " + this.clickCount + " кликов.";
+        //     }
+        //     this.$emit('game-finished', result);
+        // },
       showMiniGameFalse(){
           this.$emit('close', true);
       }
@@ -70,21 +131,36 @@ export default {
     <div v-if="!gameStarted && !gameEnded">
             <div class="rules-content">
                 <h2>Правила игры</h2>
-                <p>Нажмите на кнопку более 100 раз за 30 секунд!</p>
+<!--                <p>Нажмите на кнопку более 100 раз за 30 секунд!</p>-->
+                    <p>Запиши такие же слова на время!</p>
                 <div class="super-little-button" v-if="!gameStarted && !gameCountdownVisible" @click="startGame"><a>Старт</a></div>
             </div>
         </div>
 
-        <div v-if="!gameStarted && gameCountdownVisible">
+<!--        <div v-if="!gameStarted && gameCountdownVisible">-->
+<!--            <h1 id="countdown-number">{{ countdown }}</h1>-->
+<!--        </div>-->
+        <div v-if="!startGame && gameCountdownVisible">
             <h1 id="countdown-number">{{ countdown }}</h1>
         </div>
 
-        <div v-if="gameStarted" id="game">
-            <h2>Время: <span>{{ timeLeft }}</span> секунд</h2>
-            <h2>Клики: <span>{{ clickCount }}</span></h2>
-            <div class="little-button" @click="registerClick"><a>Нажми меня!</a></div>
+<!--        <div v-if="gameStarted" id="game">-->
+<!--            <h2>Время: <span>{{ timeLeft }}</span> секунд</h2>-->
+<!--            <h2>Клики: <span>{{ clickCount }}</span></h2>-->
+<!--            <div class="little-button" @click="registerClick"><a>Нажми меня!</a></div>-->
+<!--        </div>-->
+        <div v-if="!gameOver">
+          <p>Слово: <strong>{{ currentWord }}</strong></p>
+          <input v-model="userInput" @keyup.enter="checkWord" placeholder="Введите слово..." />
+          <p>Осталось времени: {{ timeRemaining }} сек</p>
         </div>
-    <h2 v-if="gameEnded">{{ resultMessage }}<div class="little-button" @click="showMiniGameFalse"><a>Close</a></div></h2>
+        <div v-else>
+          <h2>Результаты</h2>
+          <p>Успешно введено слов: {{ correctCount }}/5</p>
+          <div class="little-button" @click="showMiniGameFalse"><a>Close</a></div>
+        </div>
+
+<!--    <h2 v-if="gameEnded">{{ resultMessage }}<div class="little-button" @click="showMiniGameFalse"><a>Close</a></div></h2>-->
   </div>
   <div class="overlay"></div>
 </template>
@@ -256,6 +332,20 @@ export default {
   height: 100%;
   background: rgba(0, 0, 0, 0.5);
   z-index: 5;
+}
+input {
+  width: 80%;
+  padding: 15px;
+  border: 2px solid #d6c6dd;
+  margin: 15px;
+  background: #6c3282;
+  color: #d6c6dd;
+  font-family: 'Press Start 2P', sans-serif;
+  font-size: 14px;
+  text-align: center;
+  -webkit-border-radius: 5px;
+  -moz-border-radius: 5px;
+  border-radius: 5px;
 }
 
 </style>
